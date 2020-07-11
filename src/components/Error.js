a904013a6c0b9f7336fd6c72c432158d7ac8a604
souviks72 <souvikmandalvit2016@gmail.com>
2020-07-11 21:46:39 +0530

    Basic working app created

---
+++ src/components/Error.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Error = (props) => {
+    return (
+        <div className='error'>{props.error}</div>
+    )
+};
+
+export default Error;
---
+++ src/components/Main.js
@@ -0,0 +1,48 @@
+import React, {useState} from 'react';
+import axios from "axios";
+
+import Context from '../Context';
+
+import Header from './Header';
+import Content from './Content';
+import WeatherSearch from './WeatherSearch';
+import WeatherData from './WeatherData';
+import Error from './Error';
+
+const Main = () => {
+    const [weather, setWeather] = useState();
+    const [city, setCity] = useState();
+    const [error,setError] = useState();
+
+    const api_call = async (e) => {
+        e.preventDefault();
+        const location = e.target.elements.location.value;
+        if(location===''){
+            setError("Please Enter Name of a City");
+            setWeather(null);
+            return;
+        } 
+        const API_KEY = 'b1d4ae4de248a2bd1dd8f6fc57931244';
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${API_KEY}&units=metric`;
+        const request = axios.get(url);
+        const response = await request;
+        setError(null);
+        setWeather(response.data.main);
+        setCity(response.data.name);
+    }
+
+    return (
+        <div className='main'>
+            <Header/>
+            <Content>
+                <Context.Provider value={{ api_call, weather, city}}>
+                    <WeatherSearch/>
+                    { weather &&  <WeatherData/> }
+                    { error && <Error error={error}/> }
+                </Context.Provider>
+            </Content>
+        </div>    
+    )
+}
+
+export default Main;
